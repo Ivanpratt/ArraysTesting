bcf2c5fdbf69d6d82b9cc8bda0d89ebbf3324219
Ivan Pratt <ivandoublejr@gmail.com>
2022-02-10 12:41:01 -0700

    initial commit, some code added

---
+++ arraystesting.js
@@ -0,0 +1,5 @@
+let testarray = ['ivan', 'nobody', 'ethan', 'guy1', 'guy2']
+testarray.splice(2, 0, 'Jackson', 'michael', 'jordan')
+let newarray = testarray.slice(2, 0)
+console.log(testarray)
+
---
+++ index.js
@@ -0,0 +1,26 @@
+let hamburgerBackpack = []
+
+hamburgerBackpack.push("Bagel Sandwhich")
+hamburgerBackpack.push("Row Boat")
+hamburgerBackpack.push("Mr. Gassy")
+
+hamburgerBackpack.splice(1, 0, "Jeddy")
+
+let coat = "Pink Bomberjacket"
+hamburgerBackpack.push(coat)
+
+hamburgerBackpack.pop()
+
+hamburgerBackpack.push("Extra Shoes", "Juice Boxes", "First Aid Kid", "Camera", "Garnet's cat")
+
+let hotdogDuffelbag = hamburgerBackpack.splice(2, 3)
+
+
+let itemCount = hamburgerBackpack.length
+console.log(itemCount)
+console.log("Hamburgerbackpack", hamburgerBackpack)
+console.log("hotdogduffelbag", hotdogDuffelbag)
+
+for (i=0; i < hotdogDuffelbag.length; i++) {
+    console.log(hotdogDuffelbag[i])
+}
